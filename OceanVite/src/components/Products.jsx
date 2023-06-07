403fc96c8b7be81573441f3f4b53614a8803a4ff
Arjun <yrarjun59@gmail.com>
2023-06-07 07:26:33 +0545

    remove and update

--- OceanVite/src/components/Products.jsx
+++ OceanVite/src/components/Products.jsx
@@ -90,63 +90,10 @@ function Products() {
           </span>
           {/* </div> */}
         </div>
-        {/* 1 images */}
-        <div className="relative h-[350px] w-[200px] rounded-[10px]">
-          {/* images */}
-          <img
-            src="./images/bg.jpg"
-            alt="book image"
-            className="rounded-tr-[10px] rounded-tl-[10px] h-[270px] w-[200px]"
-          />
-          {/* description contaienr */}
-          <div className="flex flex-col bg-white">
-            <h4 className="font-bold text-center text-textColor w-auto">
-              Bhagwat Gita
-            </h4>
-            <p className="text-center text-textColor">Rs 19.99</p>
-            <button className="h-[30px] bg-[#FF7A5C] text-white font-bold  rounded-bl-[10px] rounded-br-[10px]">
-              Add to Cart
-            </button>
-          </div>
-
-          {/* <div className="absolute top-12"> */}
-          <span className="absolute font-bold text-white bg-pinkColor top-0 left-0">
-            4.5(199)
-          </span>
-          <span className="absolute font-bold text-white bg-pinkColor bottom-0 right-0 w-[70px] p-2">
-            Nepali
-          </span>
-          {/* </div> */}
-        </div>
-        <div className="relative h-[350px] w-[200px] rounded-[10px]">
-          {/* images */}
-          <img
-            src="./images/bg.jpg"
-            alt="book image"
-            className="rounded-tr-[10px] rounded-tl-[10px] h-[270px] w-[200px]"
-          />
-          {/* description contaienr */}
-          <div className="flex flex-col bg-white">
-            <h4 className="font-bold text-center text-textColor w-auto">
-              Bhagwat Gita
-            </h4>
-            <p className="text-center text-textColor">Rs 19.99</p>
-            <button className="h-[30px] bg-[#FF7A5C] text-white font-bold  rounded-bl-[10px] rounded-br-[10px]">
-              Add to Cart
-            </button>
-          </div>
-
-          {/* <div className="absolute top-12"> */}
-          <span className="absolute font-bold text-white bg-pinkColor top-0 left-0">
-            4.5(199)
-          </span>
-          <span className="absolute font-bold text-white bg-pinkColor bottom-0 right-0 w-[70px] p-2">
-            Nepali
-          </span>
-          {/* </div> */}
-        </div>
       </div>
-      <button className="bg-pinkColor p-2 rounded-[5px] text-white font-bold absolute right-0 bottom-5 ">Add Book</button>
+      <button className="bg-pinkColor p-2 rounded-[5px] text-white font-bold absolute right-0 bottom-5 ">
+        Add Book
+      </button>
     </div>
   );
 }
